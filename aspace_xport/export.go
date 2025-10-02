0927d405305942b9ceed93a6cb11ab41c63c0a13
Donald Mennerich <don.mennerich@nyu.edu>
2025-10-02 15:20:22 -0400

    removing timeout

--- aspace_xport/export.go
+++ aspace_xport/export.go
@@ -141,17 +141,18 @@ func exportChunk(resourceInfoChunk []ResourceInfo, resultChannel chan []ExportRe
 }
 
 func exportMarc(info ResourceInfo, res aspace.Resource, workerID int) ExportResult {
+	startTime := time.Now()
+
 	var marcBytes []byte
 	var err error
 	//get the marc record
 	marcBytes, err = client.GetMARCAsByteArray(info.RepoID, info.ResourceID, exportOptions.UnpublishedNotes)
 	if err != nil {
-		PrintAndLog(fmt.Sprintf("[worker %d] could not retrieve %s as marc xml, code: %s, retrying", workerID, res.URI, err.Error()), WARNING)
-		marcBytes, err = client.GetMARCAsByteArray(info.RepoID, info.ResourceID, exportOptions.UnpublishedNotes)
-		if err != nil {
-			PrintAndLog(fmt.Sprintf("[worker %d] could not retrieve %s as marc xml on 2nd attempt, code: %s", workerID, res.URI, err.Error()), ERROR)
-			return ExportResult{Status: "ERROR", URI: res.URI, Error: err.Error()}
-		}
+		errorTime := time.Since(startTime)
+		minutes := int(errorTime / time.Minute)
+		seconds := int((errorTime % time.Minute) / time.Second)
+		PrintAndLog(fmt.Sprintf("[worker %d] could not retrieve %s as marc xml, code: %s, time: %d:%d", workerID, res.URI, err.Error(), minutes, seconds), ERROR)
+		return ExportResult{Status: "ERROR", URI: res.URI, Error: err.Error()}
 	}
 
 	//create the output filename
